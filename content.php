dfd81b9a6faf5a2eb22ad1e16e55401dbcc9860b
Ryan Wiemer <ryan@ryanwiemer.com>
2014-03-18 01:19:12 -0700

    Adding in Advanced Custom Fields Functionality

--- content.php
+++ content.php
@@ -6,9 +6,33 @@
 
 <article <?php post_class(); ?>>
 <div class="hero" style="background-image: url('<?php the_field('hero_image'); ?>');">
-    <h2 class="hero-title"><?php the_field('hero_title'); ?></h2>
-    <h3 class="hero-subtitle"><?php the_field('hero_subtitle'); ?></h3>
+    <h2 class="hero__title"><?php the_field('hero_title'); ?></h2>
+    <h3 class="hero__subtitle"><?php the_field('hero_subtitle'); ?></h3>
+    <?php if( have_rows('hero_cta') ): ?>
+    <?php while ( have_rows('hero_cta') ) : the_row();?>
+    <a class="hero__cta" href="<?php the_sub_field('hero_cta_link');?>"><?php the_sub_field('hero_cta_text');?></a>
+    <?php endwhile; else : endif; ?>
+
+    <?php if( have_rows('sections') ): ?>
+    <?php $i=1; ?>
+    <?php while ( have_rows('sections') ) : the_row();?>
+    <ul class="hero__links">
+      <li><a href="#section-<?php echo $i++;?>"><?php the_sub_field('section_label');?></a></li>
+    </ul>
+    <?php endwhile; else : endif; ?>
+
 </div>
+
+
+<?php if( have_rows('sections') ): ?>
+<?php $i=1; ?>
+<?php while ( have_rows('sections') ) : the_row();?>
+<div id="section-<?php echo $i++;?>">
+  <?php the_sub_field('section_content');?>
+</div>
+<?php endwhile; else : endif; ?>
+
+
 
   <div class="entry-content">
     <?php the_content(); ?>
