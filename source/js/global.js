6dd0d5180ecbd7993e39cb36ef114a1d581568a1
Ryan Wiemer <ryan@ryanwiemer.com>
2014-03-25 17:52:06 -0700

    Edits to copy, styles and nearly finished with navigation highlighting

--- source/js/global.js
+++ source/js/global.js
@@ -141,7 +141,10 @@ https://github.com/imakewebthings/jquery-waypoints/blob/master/licenses.txt
 Add needed scripts here.
 
 */
-
+var curNav,
+		offset = 1,
+		animating = false,
+		$ = jQuery;
 
 (function( $ ) {
 		"use strict";
@@ -164,6 +167,48 @@ Add needed scripts here.
 								$(".section-nav__links").sticky({getWidthFrom: '.content'});
 
 							//Add jQuery Waypoints here
+
+							$('#undefined-sticky-wrapper ul li a').click(function(e){
+								e.preventDefault();
+								if(!animating) {
+									var id = $(this).attr("href"),
+										target = $(id).offset().top - offset;
+
+									animating = true;
+
+									$('html, body').animate({
+										scrollTop: target
+									}, 500, function() {
+										setTimeout(function() {
+											animating = false;
+										}, 100);
+									});
+
+									$('.current').removeClass('current');
+									$(this).addClass('current');
+								}
+							});
+
+							$(window).scroll(function() {
+								if(!animating) {
+									var nextCurNav;
+
+									$('#undefined-sticky-wrapper ul li a').each(function() {
+										var thisNav = $(this),
+											id = thisNav.attr("href");
+
+										if($(id).offset().top - offset < $(window).scrollTop()) {
+											nextCurNav = thisNav;
+										}
+									});
+
+									if((typeof nextCurNav !== "undefined" && typeof curNav === "undefined") || (typeof nextCurNav !== "undefined" && curNav.attr('href') != nextCurNav.attr('href'))) {
+										$('.current').removeClass('current');
+										nextCurNav.addClass('current');
+										curNav = nextCurNav;
+									}
+								}
+							});
 
 		});
 
