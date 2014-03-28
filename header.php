4f1e4cad4bb1db04496d52d2e10bb15072789a5c
Ryan Wiemer <ryan@ryanwiemer.com>
2014-03-28 09:57:03 -0700

    Friday morning prod push for Jordan

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en" class="no-js">
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=1000">
+<meta name="description" content="<?php echo get_option( 'blogdescription' ); ?>" />
 <link rel="icon" href="<?php echo get_template_directory_uri(); ?>//favicon.ico" type="image/x-icon" />
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>//favicon.ico" type="image/x-icon" />
 <title><?php wp_title( '|', true, 'right' ); ?> Doctor Mindy</title>
